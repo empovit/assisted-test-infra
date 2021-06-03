f7dee30313f28398493d5ff3151b60a4d9825655
Vitaliy Emporopulo <vemporop@redhat.com>
2021-06-03 13:36:07 +0300

    change order of networks to IPv6 first

--- discovery-infra/test_infra/helper_classes/kube_helpers/agent_cluster_install.py
+++ discovery-infra/test_infra/helper_classes/kube_helpers/agent_cluster_install.py
@@ -55,7 +55,7 @@ class AgentClusterInstall(BaseCustomResource):
         **kwargs,
     ) -> None:
         metadata = self.ref.as_dict()
-        metadata.update({"annotations": {"agent-install.openshift.io/install-config-overrides": "{\"networking\": {\"networkType\": \"OVNKubernetes\", \"machineNetwork\": [{\"cidr\": \"192.168.126.0/24\"}, {\"cidr\": \"1001:db8::/120\"}], \"clusterNetwork\": [{\"cidr\": \"10.128.0.0/14\", \"hostPrefix\": 23}, {\"cidr\": \"fd01::/48\", \"hostPrefix\": 64}], \"serviceNetwork\": [\"172.30.0.0/16\", \"fd02::/112\"]}}"}})
+        metadata.update({"annotations": {"agent-install.openshift.io/install-config-overrides": "{\"networking\": {\"networkType\": \"OVNKubernetes\", \"machineNetwork\": [{\"cidr\": \"1001:db8::/120\"}, {\"cidr\": \"192.168.126.0/24\"}], \"clusterNetwork\": [{\"cidr\": \"fd01::/48\", \"hostPrefix\": 64}, {\"cidr\": \"10.128.0.0/14\", \"hostPrefix\": 23}], \"serviceNetwork\": [\"fd02::/112\", \"172.30.0.0/16\"]}}"}})
         body = {
             "apiVersion": f"{self._api_group}/{self._api_version}",
             "kind": self._kind,
